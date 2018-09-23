84f62e065c3f1f9591e7a872772e079c8802a3ee
stefanks <solntsev@gmail.com>
2018-09-23 00:42:34 -0500

    lots

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,62 @@ import React, { Component } from "react";
 import MadMusicContract from "./contracts/MadMusic.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
+import Modal from 'react-modal';
 
 import "./App.css";
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { 
+      storageValue: 0,
+      web3: null,
+      accounts: null, 
+      contract: null, 
+      songIDinTextBox: '',
+      songs:[],
+      songsLeft: [],
+      modalIsOpen: false,
+      donateModalIsOpen: false,
+      selectedSong: null,
+      donationAmount: 0
+    };
+
+  openAdminModal(selectedSong) {
+    this.setState({
+      modalIsOpen: true,
+      selectedSong: selectedSong
+    });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  openDonateModal(selectedSong) {
+    this.setState({
+      donateModalIsOpen: true,
+      selectedSong: selectedSong
+    });
+  }
+
+
+  closeDonateModal() {
+    this.setState({donateModalIsOpen: false});
+  }
 
   componentDidMount = async () => {
     try {
@@ -33,6 +84,36 @@ class App extends Component {
     }
   };
 
+  handleSetCreatorsClick = async () => {
+    console.warn("in handler");
+    const { accounts, contract } = this.state;
+    if(!this.state.creators || !this.state.percents)
+    {     
+      this.closeModal();
+      return;
+    }
+    const creatorArray = this.state.creators.split(",");
+    const percentArray = this.state.percents.split(",");
+    console.warn(this.state.selectedSong);
+    console.warn(creatorArray);
+    console.warn(percentArray);
+    await contract.setCreators(this.state.selectedSong, creatorArray, percentArray, {from: accounts[0], gas: 999999});
+    this.closeModal();
+    window.location.reload()
+  }
+
+  handleDonateClick = async () => {
+    const { accounts, contract } = this.state;
+    await contract.donate(this.state.selectedSong, {from: accounts[1], value: this.state.donationAmount});
+    this.closeModal();
+    window.location.reload()
+  }
+
+  handleNewDonateClick = async () => {
+    const { accounts, contract } = this.state;
+    this.openDonateModal(this.state.songIDinTextBox)
+  }
+
   runExample = async () => {
     const { accounts, contract } = this.state;
 
@@ -42,45 +123,110 @@ class App extends Component {
 
     const response = await contract.percentToAdmin();
 
+    const numberOfSongs = await contract.getNumberOfSongs();
+
+    console.warn("number of songs: "+numberOfSongs.toNumber())
+
+    var songIDs = [];
+
+    for(var i = 0; i < numberOfSongs; i++) 
+    {
+      console.warn("i: "+i)
+      songIDs.push(await contract.getSongID(i)) 
+    }
+
+    console.warn("songIDs: "+songIDs);
+
+    for(var i = 0; i < numberOfSongs; i++) 
+    {
+      var huh = await contract.getSong(songIDs[i])
+      console.warn([huh[0].toNumber(), huh[3].toNumber()])
+      this.state.songs.push({key:songIDs[i], value:[huh[0].toNumber(), huh[1], huh[2],huh[3].toNumber()]})
+    }
+
+    this.state.songsLeft = this.state.songs;
+
     // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
+    this.setState({ storageValue: response.toNumber(), songIDs: songIDs });
   };
+
+  handleChangeCreators(event) {
+    this.setState({creators: event.target.value})
+  }
+
+  handleChangePercents(event) {
+    this.setState({percents: event.target.value})
+  }
+
+  handleChangeDonationAmount(event) {
+    this.setState({donationAmount: event.target.value})
+  }
+
+  getUnclaimedString(value){
+    if (value==0)
+      return "All Proceeds Distributed";
+    else
+      return value;
+  }
+
+  onChangeHandler(e){
+    this.setState({
+      songIDinTextBox: e.target.value,
+      songsLeft: this.state.songs.filter(d => e.target.value === '' || d.key.includes(e.target.value))
+    })
+  }
 
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+
+    const dynamicList = this.state.songsLeft
+        .map((d, index) =>
+        <div>
+         <li key={index}>{d.key} {this.getUnclaimedString(d.value[0])} {d.value[1][0]} Total Donated:{d.value[3]}</li>
+         {this.state.adminMode && <button onClick={() => this.openAdminModal(d.key)}>Set Creators</button>}
+         {!this.state.adminMode && <button onClick={() => this.openDonateModal(d.key)}>Donate!</button>}
+        </div>);
     return (
-
-    <div class = "container">
-        <div class="jumbotron text-center">
-          <h1 id="title">MadMusic</h1>
-        </div>
-          <div>Percent going to MadMusic admins is: {this.state.storageValue}</div>
-
-      <div class="col-md-12" id="article-list">
-          <div class="row">
-            <div class="col-lg-12">
-              <p id="account" class="welcome pull-right"></p>
-              <p id="accountBalance" class="welcome pull-left"></p>
-            </div>
+    <div>
+        <h1 id="title">MadMusic</h1>
+        <div>Percent going to MadMusic admins is: {this.state.storageValue}</div>
+        <button onClick={() => this.setState({ adminMode: !this.state.adminMode })}>Toggle Admin Mode</button>
+          <div>
+          Song:  <input value={this.state.songIDinTextBox} type="text" onChange={this.onChangeHandler.bind(this)}/>
+          <button onClick={() => this.handleNewDonateClick()} disabled={this.state.songsLeft.length>0}>Add Song!</button>
+            <ul>{dynamicList}</ul>
           </div>
 
-        <div class="row panel panel-default">
-          <div class="panel-heading clearfix" id="title-chunk">
-            <div class="panel-title">
-              <button id="btn-top-left" class="btn btn-lg pull-left" data-toggle="collapse" data-target="#events" aria-expanded="false" aria-controls="events">Events</button>
-              <button id="btn-top-right" class="btn btn-lg pull-right" data-toggle="modal" data-target="#sellArticle">Sell an article</button>
-            </div>
-          </div>
-          <ul id="events" class="collapse list-group"></ul>
-        </div>
+          {/*  Admin modal */}
+          <Modal
+          isOpen={this.state.modalIsOpen}
+          style={customStyles}>
+          <div className="panel-title3">
+                Creators: <input type="text" value={this.state.creators} onChange={this.handleChangeCreators.bind(this)}/>
+                Percents: <input type="text" value={this.state.percents} onChange={this.handleChangePercents.bind(this)}/>
+                <button onClick={()=>this.handleSetCreatorsClick()}>Set Creators</button>
+                </div>
+                <div className="bottom-buttons">
+                <button onClick={()=>this.closeModal()}>Cancel</button>
+                <button onClick={()=>this.handleSetCreatorsClick()}>Set Creators</button>
+                </div>
+          </Modal>
 
-        <div id="articlesRow" class="row">
-          
+          {/*  Donate modal */}
+          <Modal
+          isOpen={this.state.donateModalIsOpen}
+          style={customStyles}>
+          <div className="panel-title3">
+                Donation Amount: <input type="text" value={this.state.donationAmount} onChange={this.handleChangeDonationAmount.bind(this)}/>
+                </div>
+                <div className="bottom-buttons">
+                <button onClick={()=>this.closeDonateModal()}>Cancel</button>
+                <button onClick={()=>this.handleDonateClick()}>Donate!</button>
+                </div>
+          </Modal>
         </div>
-      </div>
-    </div>
     );
   }
 }
--- test/simplestorage.js
+++ test/simplestorage.js
@@ -4,15 +4,31 @@ contract("MadMusic", accounts => {
   it("...should record donation of 3 to song 24.", async () => {
     const madMusicInstance = await MadMusic.deployed();
 
-    await madMusicInstance.donate(24, {value: 3});
+    await madMusicInstance.donate("24", {value: 3});
         
-    const ret = await madMusicInstance.getSong(24);
-
-    console.warn(ret)
+    const ret = await madMusicInstance.getSong("24");
 
     const noice = ret[0];
 
     assert.equal(noice, 3, "3 has been donated to song 24.");
 
+  });
+  it("...should record donation of 4000 to song 25.", async () => {
+    const madMusicInstance = await MadMusic.deployed();
+
+    await madMusicInstance.donate("25", {value: 4000});
+        
+    var ok = ["0x0d1d4e623d10f9fba5db95830f7d3839406c6af2", "0x821aea9a577a9b44299b9c15c88cf3087f3b5544"];
+    var ok2 = [40,40];
+
+    console.warn("Right before setCreators!");
+    await madMusicInstance.setCreators("25", ok, ok2, {gas: 999999});
+
+    const ret = await madMusicInstance.getSong("25");
+
+    console.warn(ret[0].toNumber());
+    console.warn(ret[1]);
+    console.warn(ret[2]);
+    console.warn(ret[3].toNumber());
   });
 });
